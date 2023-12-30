a0891c0190bfcc2e26553293345e17e62ad31896
ansarcodes <128506717+ansarcodes@users.noreply.github.com>
2023-12-30 21:46:28 +0600

    Add full logic Pre-Final draft

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 const container = document.querySelector("#container");
-for (let i = 0; i < 16 * 16; i++) {
-    let square = document.createElement("div");
-    square.classList.add('square');
-    container.appendChild(square);
-    square.addEventListener("mouseover", () => {
-        square.style = "background-color: red";
-    })
+const input = document.querySelector("input");
+const buttonSet = document.querySelector("button#set");
+const buttonReset = document.querySelector("button#reset");
+const allChildren = document.querySelectorAll(".square");
+
+
+function setGrid() {
+    container.replaceChildren();
+    let numberOfSquares = Number(input.value);
+    let squareFlexBasis = 640 / numberOfSquares;
+    for (let i = 0; i < numberOfSquares ** 2; i++) {
+        let square = document.createElement("div");
+        square.classList.add('square');
+        square.style.flexBasis = squareFlexBasis + "px";
+        container.appendChild(square);
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "red";
+        })
+    }
+    
 }
+
+setGrid();
+
+buttonSet.addEventListener("click", () => {
+    setGrid();
+})
